ac755a70066dad5739847a9e1784c5195522082b
임지 <imjiyoung@daum.net>
2023-04-28 21:59:13 +0900

    2일차 코팅

---
+++ day02/src/JavaStructure.java
@@ -0,0 +1,21 @@
+//[자바의 문장 특징]
+
+//1. 자바는 클래스이름과 파일명이 같아야 한다, 틀리면 에러난다
+//2.자바는 대소문자를 구분해야한다
+//ex.Java,java 는 같지않다 
+//3.{} 중괄호로 문단,문장을 구분한다.
+//4. 자바의 문장의 끝은 ;을 사용한다.
+
+
+public class JavaStructure 
+{
+    public static void main (String args[]) {
+    	//System.out.println("하하하");
+    	//System.out.println("하하하")
+    	//문장의 끝에는 ;이 필요해서 오류
+    System
+    .out
+    .println("허허허")    ;
+    }
+}
+
