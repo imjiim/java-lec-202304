0128c3195bd041eac418dce3fc1b240e206d2925
임지 <imjiyoung@daum.net>
2023-05-02 21:16:58 +0900

    3일차

--- day02/src/JavaStructure.java
+++ day02/src/JavaStructure.java
@@ -5,17 +5,19 @@
 //ex.Java,java 는 같지않다 
 //3.{} 중괄호로 문단,문장을 구분한다.
 //4. 자바의 문장의 끝은 ;을 사용한다.
+//컴파일 에러를 일으키는 기초 문법
 
 
-public class JavaStructure 
+public class JavaStructure
 {
     public static void main (String args[]) {
     	//System.out.println("하하하");
     	//System.out.println("하하하")
     	//문장의 끝에는 ;이 필요해서 오류
     System
-    .out
+    .out //<--에러 ,문장이 중간에 끝났다
     .println("허허허")    ;
     }
 }
+
 
---
+++ day03/src/day03/VariableExamlpe.java
@@ -0,0 +1,27 @@
+package day03;
+
+public class VariableExamlpe {
+	
+	public static void main(String args[]) {
+	
+		int  age;//<--변수 선언
+		//자료형 변수명
+		age = 40;//<--변수 초기화 (변수 대입)
+		int age2 =20;// <--변수 선언과 동시에 초기화
+		
+		//int age;// 같은 이름의 변수는 선언할수없다 (컴파일에러)
+		
+		//--변수를 사용한 부분 
+		int ageSum = age + age2;
+		System.out.println("age + age2="+ ageSum);
+
+		/*자바 예약어는 변수명으로 쓸수 없다 
+		 * int if;
+		 * int for
+		 * int break;
+		 */
+		
+		
+	}
+	
+}
